9967cc2a43c6a9a106a104b0c354efe77c0236c5
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-05 09:20:27 +0100

    gcc 4.9.1

---
+++ cross/lib/gcc/i686-elf/4.9.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.1/configure --target=i686-elf --prefix=/home/rhu/opt/cross --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
