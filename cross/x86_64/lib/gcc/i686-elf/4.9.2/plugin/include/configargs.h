a8566cd60b537a13db26e9909019908107ed2f94
Richard Hull <rm_hull@yahoo.co.uk>
2014-12-27 15:18:06 +0000

    Update 64-bit gcc to 4.9.2

---
+++ cross/x86_64/lib/gcc/i686-elf/4.9.2/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --target=i686-elf --prefix=/home/rhu/i686-elf/cross/x86_64/ --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "pentiumpro" } };
