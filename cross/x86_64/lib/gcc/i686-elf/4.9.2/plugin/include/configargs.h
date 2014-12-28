c29d389cacd70585b13e43815f0edefaa6a44afc
Richard Hull <rm_hull@yahoo.co.uk>
2014-12-28 22:36:18 +0000

    Stripped too much

--- cross/x86_64/lib/gcc/i686-elf/4.9.2/plugin/include/configargs.h
+++ cross/x86_64/lib/gcc/i686-elf/4.9.2/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc-4.9.2/configure --target=i686-elf --prefix=/home/rhu/i686-elf/cross/x86_64/ --disable-nls --enable-languages=c,c++ --without-headers";
+static const char configuration_arguments[] = "../gcc-4.9.2/configure --target=i686-elf --prefix=/home/rhu/i686-elf/cross/x86_64 --disable-nls --enable-languages=c,c++ --without-headers";
 static const char thread_model[] = "single";
 
 static const struct {
