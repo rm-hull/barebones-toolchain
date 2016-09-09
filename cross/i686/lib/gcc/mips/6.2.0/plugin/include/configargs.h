9ad96a26624f132a37df5d0fcd6672eb5e477381
Richard Hull <rm_hull@yahoo.co.uk>
2016-09-09 01:06:10 +0100

    Update 32-bit host to latest toolchain versions for MIPS target

---
+++ cross/i686/lib/gcc/mips/6.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-6.2.0/configure --target=mips --prefix=/home/rhu/dev/barebones-toolchain/cross/i686 --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
